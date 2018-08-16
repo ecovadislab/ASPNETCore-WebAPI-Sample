e3138e32c55403a01b33338129983351b4a0c174
Aymen BEN MILED <benmiledaymen@gmail.com>
2018-08-16 17:03:09 +0200

    decrease .net core version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 dotnet restore
 dotnet build
-dotnet run --project .\SampleWebApiAspNetCore\SampleWebApiAspNetCore.csproj
+#dotnet run --project .\SampleWebApiAspNetCore\SampleWebApiAspNetCore.csproj
 #dotnet test --project .\SampleWebApiAspNetCore\SampleWebApiAspNetCore.csproj
